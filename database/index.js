590827ead812751fc45dd4d37d277dd53a62ae91
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-06 17:32:30 -0500

    Server and Database drafted

---
+++ database/index.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const config = require('../config.js');
+const db = mysql.createConnection(config);
+
+db.connect();
+
+var retrieveProduct = function (id, res) {
+  db.query(`SELECT * FROM product_options WHERE _id=${id}`, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(results[0]);
+    }
+  });
+}
+
+module.exports = {
+  db,
+  retrieveProduct
+};
