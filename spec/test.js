c3b32648b320fa5717014d715a3fd15c6b1d7d74
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-07 13:44:17 -0500

    Working on testing still

--- spec/test.js
+++ spec/test.js
@@ -1,17 +1,16 @@
-//const server = require('../server/index.js');
+const server = require('../server/index.js');
 const db = require('../database/index.js');
-//const $ = require('jquery');
 
-test('tests to make sure server is up and running', () => {
-  const $ = require('jquery');
-  return $.ajax({
-    type: 'GET',
-    url: '/',
-    success: function(data) {
-      return data;
-    }
-  }).then((data) => {
-    expect(data.status).toBe(200);
-    done();
+describe('get request with jquery', () => {
+  test('tests to make sure server is sending on get', () => {
+    const callback = (data) => {
+      try {
+        expect(data.length).not.toBe(0);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    };
+    $.get('/api/5', (data) => callback(data));
   });
 });
