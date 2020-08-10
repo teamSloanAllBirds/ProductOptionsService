3de8c17838d8d110fba2bb02e9d792aa206b1b3d
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-10 17:35:20 -0500

    Almost all functionality working on color selector

--- client/statefulButton.jsx
+++ client/statefulButton.jsx
@@ -3,11 +3,56 @@ import React from 'react';
 class StatefulButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      hover: false,
+      style: this.props.style,
+      colorway_name: this.props.colorway_name,
+      selected: false
+    }
+    this.toggleHover = this.toggleHover.bind(this);
+    this.toggleClick = this.toggleClick.bind(this);
+  }
+  toggleHover() {
+    if (!this.state.hover) {
+      this.setState({
+        hover: !this.state.hover,
+        style: {
+          display: this.state.style.display,
+          margin: this.state.style.margin,
+          width: this.state.style.width,
+          height: this.state.style.height,
+          backgroundColor: this.state.style.backgroundColor,
+          borderRadius: this.state.style.borderRadius,
+          border: "1px solid #dedede",
+          padding: "2px",
+        },
+        colorway_name: this.state.colorway_name
+      });
+    } else {
+      this.setState({
+        hover: !this.state.hover,
+        style: {
+          display: this.state.style.display,
+          margin: this.state.style.margin,
+          width: this.state.style.width,
+          height: this.state.style.height,
+          backgroundColor: this.state.style.backgroundColor,
+          borderRadius: this.state.style.borderRadius,
+          border: "1px solid white ",
+          padding: "2px"
+        },
+        colorway_name: this.state.colorway_name
+      });
+    }
+  }
+  toggleClick(e) {
+    e.preventDefault();
+    this.props.onClick(e);
   }
   render() {
     return (
-      <div style={this.props.style}>
+      <div onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} style={this.state.style} onClick={this.toggleClick} colorway_name={this.props.colorway_name}>
+        <div style={this.props.background}/>
         {this.props.image}
         {this.props.text}
       </div>
