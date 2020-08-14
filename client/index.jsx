34da7553f7b1a78a33ac4d60895da3ea5fc54961
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-14 17:59:03 -0500

    Index.jsx almost working with linting

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import PropTypes from 'prop-types';
+// import styles from 'styled-components';
+
 import ProductIntro from './productIntro.jsx';
 import ProductOrder from './productOrder.jsx';
 import ProductDetail from './productDetail.jsx';
-import styles from 'styled-components';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,36 +15,84 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    $.get('/api/5', (data) => {
-      this.setState(data);
-    }).then(() => {
-      return;
-    })
+    // $.get('/api/5', (data) => {
+    //   this.setState(data);
+    // });
   }
 
   render() {
-    return (
-      <div className="app">
-        <ProductIntro name={this.state.productName}
-        price={this.state.productPrice}
-        stars={this.state.productReviewStars}
-        reviews={this.state.productReviewQuantity}/>
-        <ProductOrder parent_state={this.state}/>
-        <span style={{display: "inline-block", fontSize: "small", marginTop: "10px", marginBottom: "10px", textAlign: "center", width: "95%"}}>Free shipping & 30-day returns, no questions asked</span>
-        <ProductDetail name={this.state.productName}
-        highlight1={this.state.highlight1Text}
-        highlight2={this.state.highlight2Text}
-        highlight3={this.state.highlight3Text}
-        core_features={this.state.coreFeatures}
-        product_description={this.state.productDescription}
-        shipping_returns={this.state.shippingAndReturns}
-        care_guide={this.state.careGuide}/>
-      </div>
-    )
+    $.get('/api/5', (data) => {
+      this.setState(data);
+    }, (data) => {
+      const {
+        productName,
+        productPrice,
+        productReviewStars,
+        productReviewQuantity,
+        highlight1Text,
+        highlight2Text,
+        highlight3Text,
+        coreFeatures,
+        productDescription,
+        shippingAndReturns,
+        careGuide,
+      } = data;
+      return (
+        <div className="app">
+          <ProductIntro
+            name={productName}
+            price={productPrice}
+            stars={productReviewStars}
+            reviews={productReviewQuantity}
+          />
+          <ProductOrder
+            parent_state={this.state}
+          />
+          <span
+            style={{
+              display: 'inline-block',
+              fontSize: 'small',
+              marginTop: '10px',
+              marginBottom: '10px',
+              textAlign: 'center',
+              width: '95%',
+            }}
+          >
+            Free shipping & 30-day returns, no questions asked
+          </span>
+          <ProductDetail
+            name={productName}
+            highlight1={highlight1Text}
+            highlight2={highlight2Text}
+            highlight3={highlight3Text}
+            coreFeatures={coreFeatures}
+            productDescription={productDescription}
+            shippingReturns={shippingAndReturns}
+            careGuide={careGuide}
+          />
+        </div>
+      );
+    }).then((data) => {
+      return data;
+    });
   }
 }
+
+// App.propTypes = {
+//   productName: PropTypes.string.isRequired,
+//   productPrice: PropTypes.number.isRequired,
+//   productReviewStars: PropTypes.number.isRequired,
+//   productReviewQuantity: PropTypes.number.isRequired,
+//   highlight1Text: PropTypes.string.isRequired,
+//   highlight2Text: PropTypes.string.isRequired,
+//   highlight3Text: PropTypes.string.isRequired,
+//   coreFeatures: PropTypes.string.isRequired,
+//   productDescription: PropTypes.string.isRequired,
+//   shippingAndReturns: PropTypes.string.isRequired,
+//   careGuide: PropTypes.string.isRequired,
+// };
+
 // bonking out the render for testing
 // adding in export default for testing
-//export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
-
+// export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
