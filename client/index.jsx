da353d2df83bedc8bf6fca7e670db787fb379190
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-15 12:53:22 -0500

    Everything is linted and working

--- client/index.jsx
+++ client/index.jsx
@@ -43,7 +43,7 @@ class App extends React.Component {
           reviews={productReviewQuantity}
         />
         <ProductOrder
-          parent_state={this.state}
+          parentState={this.state}
         />
         <span
           style={{
