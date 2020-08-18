19f353477412cebea53ae293888a1f665df8f379
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-17 19:03:31 -0500

    Change api endpoint to make it work with proxy

--- client/App.jsx
+++ client/App.jsx
@@ -14,7 +14,7 @@ class OptionsApp extends React.Component {
   }
 
   componentDidMount() {
-    $.get('/api/5', (data) => {
+    $.get('/api/productoptions/5', (data) => {
       this.setState(data);
     });
   }
